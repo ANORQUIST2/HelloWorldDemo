10bc812aec306b6952a121e0e1b0a80d83ddf6af
ANORQUIST2 <143917620+ANORQUIST2@users.noreply.github.com>
2023-09-02 22:24:47 -0500

    Add files via upload

---
+++ HelloWorldDemo2.0/HelloWorldDemo2.0/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HelloWorldDemo2.0
+//
+//  Created by Ahren on 9/2/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, Ahren!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HelloWorldDemo2.0/HelloWorldDemo2.0/HelloWorldDemo2_0App.swift
@@ -0,0 +1,17 @@
+//
+//  HelloWorldDemo2_0App.swift
+//  HelloWorldDemo2.0
+//
+//  Created by Samantha Thomas on 9/2/23.
+//
+
+import SwiftUI
+
+@main
+struct HelloWorldDemo2_0App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
